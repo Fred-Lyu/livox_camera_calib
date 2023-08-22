974c16feba3d009f7919cf9a2e109f51c5f130c8
Fred-Lyu <fredlyu0924@gmail.com>
2023-08-22 12:19:03 +0800

    add output to single frame point cloud

--- src/bag_to_pcd.cpp
+++ src/bag_to_pcd.cpp
@@ -14,6 +14,8 @@ string bag_file;
 string lidar_topic;
 string pcd_file;
 bool is_custom_msg;
+bool is_single_frame_output;
+string pcd_path;
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "lidarCamCalib");
@@ -22,7 +24,8 @@ int main(int argc, char **argv) {
   nh.param<string>("pcd_file", pcd_file, "");
   nh.param<string>("lidar_topic", lidar_topic, "/livox/lidar");
   nh.param<bool>("is_custom_msg", is_custom_msg, false);
-  pcl::PointCloud<pcl::PointXYZI> output_cloud;
+  nh.param<bool>("is_single_frame_output", is_single_frame_output, true);
+  nh.param<string>("pcd_path", pcd_path, "");
   std::fstream file_;
   file_.open(bag_file, ios::in);
   if (!file_) {
@@ -38,6 +41,9 @@ int main(int argc, char **argv) {
     ROS_ERROR_STREAM("LOADING BAG FAILED: " << e.what());
     return -1;
   }
+  if(!is_single_frame_output){
+  ROS_INFO("\033[0;31m Multi frame point cloud output\033[0m");
+  pcl::PointCloud<pcl::PointXYZI> output_cloud;
   std::vector<string> lidar_topic_vec;
   lidar_topic_vec.push_back(lidar_topic);
   rosbag::View view(bag, rosbag::TopicQuery(lidar_topic_vec));
@@ -71,5 +77,50 @@ int main(int argc, char **argv) {
   pcl::io::savePCDFileASCII(pcd_file, output_cloud);
   string msg = "Sucessfully save point cloud to pcd file: " + pcd_file;
   ROS_INFO_STREAM(msg.c_str());
+  }
+  else{
+  ROS_INFO("\033[0;31m Single frame point cloud output\033[0m");
+  std::vector<string> lidar_topic_vec;
+  lidar_topic_vec.push_back(lidar_topic);
+  rosbag::View view(bag, rosbag::TopicQuery(lidar_topic_vec));
+  for (const rosbag::MessageInstance &m : view) {
+    pcl::PointCloud<pcl::PointXYZI> output_cloud;
+    if (is_custom_msg) {
+      livox_ros_driver::CustomMsg livox_cloud_msg =
+          *(m.instantiate<livox_ros_driver::CustomMsg>()); // message type
+      for (uint i = 0; i < livox_cloud_msg.point_num; ++i) {
+        pcl::PointXYZI p;
+        p.x = livox_cloud_msg.points[i].x;
+        p.y = livox_cloud_msg.points[i].y;
+        p.z = livox_cloud_msg.points[i].z;
+        p.intensity = livox_cloud_msg.points[i].reflectivity;
+        output_cloud.points.push_back(p);
+      }
+    } else {
+      sensor_msgs::PointCloud2 livox_cloud;
+      livox_cloud = *(m.instantiate<sensor_msgs::PointCloud2>()); // message
+      pcl::PointCloud<pcl::PointXYZI> cloud;
+      pcl::PCLPointCloud2 pcl_pc;
+      pcl_conversions::toPCL(livox_cloud, pcl_pc);
+      pcl::fromPCLPointCloud2(pcl_pc, cloud);
+      for (uint i = 0; i < cloud.size(); ++i) {
+        output_cloud.points.push_back(cloud.points[i]);
+      }
+    }
+    output_cloud.is_dense = false;
+    output_cloud.width = output_cloud.points.size();
+    output_cloud.height = 1;
+    static int i=1;
+    string saveName(pcd_path);
+    saveName+=to_string(i++);
+    saveName+=".pcd";
+    pcl::io::savePCDFileASCII(saveName, output_cloud);
+    ROS_INFO_STREAM("Sucessfully save point cloud id:" + to_string(i++));
+
+  }
+
+  string msg = "Sucessfully save point cloud to pcd path: " + pcd_path;
+  ROS_INFO_STREAM(msg.c_str());
+  }
   return 0;
 }
